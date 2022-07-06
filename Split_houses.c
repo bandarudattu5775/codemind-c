07c85f5a77c189a5df5b9771d06ddd4b63d15cea
bandarudattu5775 <100119427+bandarudattu5775@users.noreply.github.com>
2022-07-06 21:23:56 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     char str[a];     scanf("%s",str);     int c=0,i;     for (i=0; i<a; i++){         if (str[i]=='.'){             c+=1;         }     }     if (c>a/2){         printf("YES ");         for (i=0; i<a; i++){             if (str[i]=='.'){                 printf("B");             }             else{                 printf("H");             }         }     }     else{         printf("NO ");     }     return 0; }

---
+++ Split_houses.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    char str[a];
+    scanf("%s",str);
+    int c=0,i;
+    for (i=0; i<a; i++){
+        if (str[i]=='.'){
+            c+=1;
+        }
+    }
+    if (c>a/2){
+        printf("YES
+");
+        for (i=0; i<a; i++){
+            if (str[i]=='.'){
+                printf("B");
+            }
+            else{
+                printf("H");
+            }
+        }
+    }
+    else{
+        printf("NO
+");
+    }
+    return 0;
+}
